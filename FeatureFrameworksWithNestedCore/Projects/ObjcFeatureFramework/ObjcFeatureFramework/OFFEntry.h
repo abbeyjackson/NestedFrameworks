47df1276d55e765c078a5e17291bb2c3d1492668
Abbey Jackson <abbey@abbeyjackson.ca>
2019-05-08 14:16:36 -0700

    NC - Add ObjcFeatureFramework

---
+++ FeatureFrameworksWithNestedCore/Projects/ObjcFeatureFramework/ObjcFeatureFramework/OFFEntry.h
@@ -0,0 +1,14 @@
+//
+//  OFFEntry.h
+//  ObjcFeatureFramework
+//
+//  Created by Abbey Jackson on 2019-05-03.
+//  Copyright © 2019 abbeytest. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OFFEntry : NSObject
+- (NSString *)objcLabel;
+@end
+
