9d7ca90f9977bfcf22fb09dd2dd94c4d29c94334
Abbey Jackson <abbey@abbeyjackson.ca>
2019-05-08 14:17:50 -0700

    NC - Add compiled simulator and iOS frameworks for NestedCoreFramework, SwiftFeatureFramework, and ObjcFeatureFramework. These are for posterity and can be used for testing in non-cocoapod set ups.

---
+++ FeatureFrameworksWithNestedCore/Frameworks/iPhone/ObjcFeatureFramework.framework/Headers/ObjcFeatureFramework.h
@@ -0,0 +1,20 @@
+//
+//  ObjcFeatureFramework.h
+//  ObjcFeatureFramework
+//
+//  Created by Abbey Jackson on 2019-05-03.
+//  Copyright © 2019 abbeytest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <ObjcFeatureFramework/OFFEntry.h>
+
+//! Project version number for ObjcFeatureFramework.
+FOUNDATION_EXPORT double ObjcFeatureFrameworkVersionNumber;
+
+//! Project version string for ObjcFeatureFramework.
+FOUNDATION_EXPORT const unsigned char ObjcFeatureFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjcFeatureFramework/PublicHeader.h>
+
+
---
+++ FeatureFrameworksWithNestedCore/Frameworks/iPhone/SwiftFeatureFramework.framework/Headers/SwiftFeatureFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFeatureFramework.h
+//  SwiftFeatureFramework
+//
+//  Created by Abbey Jackson on 2019-05-03.
+//  Copyright © 2019 abbeytest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFeatureFramework.
+FOUNDATION_EXPORT double SwiftFeatureFrameworkVersionNumber;
+
+//! Project version string for SwiftFeatureFramework.
+FOUNDATION_EXPORT const unsigned char SwiftFeatureFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFeatureFramework/PublicHeader.h>
+
+
