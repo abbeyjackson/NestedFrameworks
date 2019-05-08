ff0dd86844ba2d384a98c5afafc1ff1dd76a969e
Abbey Jackson <abbey@abbeyjackson.ca>
2019-05-08 14:12:51 -0700

    NF - Add NestedObjcFramework

---
+++ CoreFrameworkWithNestedFeatures/Projects/NestedObjcFramework/NestedObjcFramework/NOFEntry.h
@@ -0,0 +1,13 @@
+//
+//  NOFEntry.h
+//  NestedObjcFramework
+//
+//  Created by Abbey Jackson on 2019-05-02.
+//  Copyright © 2019 abbeytest. All rights reserved.
+//
+
+@import Foundation;
+
+@interface NOFEntry : NSObject
+- (NSString *)nestedObjcLabel;
+@end
---
+++ CoreFrameworkWithNestedFeatures/Projects/NestedObjcFramework/NestedObjcFramework/NOFEntry.m
@@ -0,0 +1,18 @@
+//
+//  NOFEntry.m
+//  NestedObjcFramework
+//
+//  Created by Abbey Jackson on 2019-05-02.
+//  Copyright © 2019 abbeytest. All rights reserved.
+//
+
+
+#import "NOFEntry.h"
+
+@implementation NOFEntry
+
+- (NSString *)nestedObjcLabel {
+    return @"Nested Objc Connected";
+}
+
+@end
